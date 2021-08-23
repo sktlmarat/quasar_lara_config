6d0a9195d266091ed3183fe3d77a260688667ce7
Turlubayev Marat <marat.turlubayev@nu.edu.kz>
2021-08-23 22:58:15 +0600

    initial setup

---
+++ api/app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return response('success', 200);
+        }
+
+        return response('error', 401);
+    }
+}
---
+++ app/src/pages/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <q-btn label="Login" @click="login"></q-btn>
+    <q-btn label="Get User" @click="getUser"></q-btn>
+
+  </q-page>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'PageIndex',
+  mounted() {
+    axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie/')
+    .then(res => {
+      console.log(res)
+    })
+  },
+  methods: {
+    login () {
+      const form = {
+        email: 'luke@gmail.com',
+        password: 'secret'
+      }
+      axios.post('http://127.0.0.1:8000/login', form)
+    },
+    getUser () {
+      axios.get('http://127.0.0.1:8000/api/user/')
+        .then(res => {
+          alert([res.data])
+          console.log(res.data)
+        })
+    }
+  }
+}
+</script>
